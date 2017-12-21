bbcd13e5384b83d13b7eb7fc9f457eda33d7c443
Jin Zhu <jinz1@andrew.cmu.edu>
2017-12-21 12:02:29 -0500

    test reference

--- Code/pointerReference/main.cpp
+++ Code/pointerReference/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
+
+void testReference(){
+    //reference:
+    int ival = 1024;
+    int &refVal = ival; //refVal refers to (is another name for ) ival
+    //int &refVal2; //error, a reference must be initialized
+    cout<<"ival: "<<ival<<endl;
+    cout<<"refVal: "<<refVal<<endl;
+
+    refVal = 100;
+    cout<<"ival: "<<ival<<endl;
+    cout<<"refVal: "<<refVal<<endl;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
+    testReference();
     return 0;
 }
