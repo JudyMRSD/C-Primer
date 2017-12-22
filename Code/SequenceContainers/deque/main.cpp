629a70d1c10829c91a9397521a98377b499a74d7
Jin Zhu <jinz1@andrew.cmu.edu>
2017-12-21 21:33:41 -0500

    deque

--- Code/SequenceContainers/deque/main.cpp
+++ Code/SequenceContainers/deque/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#import <deque>
+using namespace std;
+
+void testDeque(){
+
+
+}
+
+size_t InsertionIndex(deque<int>& values, int val){
+    for (int i = 0; i<values.size(); i++){
+        if (val<values[i]){
+            return i;
+        }
+    }
+    //if val is larger than any number in the vector
+    return values.size();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int kNumValues = 5;
+    deque<int> values;
+    int val;
+    int index;
+    for (int i = 0; i<kNumValues; i++){
+        val = rand()%10;
+        index = InsertionIndex(values, val);
+        values.insert(values.begin()+index, val);
+
+    }
+    for (int i = 0; i<values.size(); i++){
+        cout<<values[i]<<endl;
+    }
+
+
+
     return 0;
 }
